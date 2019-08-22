95d9e437e6a06323162ab97c71602271f17b2f8d
OscarTorres12 <54143880+OscarTorres12@users.noreply.github.com>
2019-08-22 09:15:36 -0500

    Crear Categoria

---
+++ AppWebProductoCF/Models/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppWebProductoCF.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Required(ErrorMessage = "Ingrese un nombre.")]
+        [StringLength(50, MinimumLength = 3,
+            ErrorMessage = "El nombre no debe tener más de 50 caracteres")]
+
+        public string Nombre { get; set; }
+        [StringLength(255,
+            ErrorMessage = "La descripción no debe tener mas de 255 caracteres")]
+        [Display(Name = "Descripción")]
+
+        public string Descripcion { get; set; }
+        public bool? Estado {get; set;}
+        public virtual ICollection<Producto> Producto { get; set; }
+    }
+}
